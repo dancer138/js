ff2f7e8789111e3aea9ae77c557a81cd7e1d9735
dancer138 <lenovotab2020new@gmail.com>
2026-01-16 22:17:05 +0300

    start
    ready?

---
+++ js.js
@@ -0,0 +1,25 @@
+ let age =  "Вы принимаете файлы  cookie!";
+    alert(age); // выводит "undefined"
+    let password;
+    do {
+        password = prompt("Введите пароль:");
+    } while (password !== "1234");
+    alert ("подтверждено");
+    console.log("пароль принят!");
+
+    let x = 10;
+    let y = 5;
+    let sum = x + y;
+    console.log(sum);
+
+    let name = " i love JS";
+    console.log(name);
+
+    let n = 7;
+    let sum2 = n*2;
+    console.log(sum2);
+
+    let message =" ";
+
+ 
+    
